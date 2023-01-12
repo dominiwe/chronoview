969919380e3b6cb7bf44d2b0635a6e26239d1bad
Domi <dominic@domdeck.ch>
2023-01-12 15:47:17 +0100

    testing localstorage store

--- src/lib/components/SharedComponentTest/store.ts
+++ src/lib/components/SharedComponentTest/store.ts
@@ -1,3 +1,23 @@
 import { writable } from "svelte/store";
+import { browser } from "$app/environment";
 
-export const count = writable(0);
+let stored = 0;
+console.log("first", stored);
+
+// get count from localstorage
+if (browser) {
+    const retreived = parseInt(localStorage.counter);   
+    if (retreived && !Number.isNaN(retreived)) {
+        stored = retreived;
+    }
+    console.log("second", stored);
+}
+
+// sane default 0
+export const count = writable(stored);
+
+// update localstorage when store updates
+if (browser) {
+    count.subscribe((value) => localStorage.count = value);
+    console.log("third", stored);
+}
