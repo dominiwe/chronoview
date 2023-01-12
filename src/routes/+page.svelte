47afba9b2f39a11d5b77d82c5b8ee3dab5f87fc2
Domi <dominic@domdeck.ch>
2023-01-12 20:40:22 +0100

    test aw client cors

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,13 @@
 <script lang="ts">
 	import { CalendarWrapper, DexieTest, SharedComponentTest } from '$lib/components';
+	import { AWClient } from 'aw-client';
+	import { onMount } from 'svelte';
+
+	onMount(async () => {
+		const client = new AWClient('test-client');
+		const info = await client.getInfo();
+		console.log(info);
+	});
 </script>
 
 <SharedComponentTest />
